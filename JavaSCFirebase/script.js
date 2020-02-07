38b8022a70f173cf281cf6c37a2a733907515eaa
JavierYucraZ <xavwlr20@gmail.com>
2020-02-06 20:32:59 -0400

    db

--- JavaSCFirebase/script.js
+++ JavaSCFirebase/script.js
@@ -57,3 +57,49 @@ function getID( id ){
   return document.getElementById(id);
 }
 
+
+function crearTabla(id, nombre, apellido){
+  return '<tr>'+
+  '<td>'+id+'</td>'+
+  '<td>'+nombre+'</td>'+
+  '<td>'+apellido+'</td>'+
+  '<td>'+
+  '<button class="btn btn-warning btn-sm" onclick="editar('+id+',\''+nombre+'\',\''+apellido+'\')">Editar</button>'+
+  '</td>'+
+  '<td>'+
+  '<button class="btn btn-danger btn-sm" onclick="eliminar('+id+',\''+nombre+'\',\''+apellido+'\')">Eliminar</button>'+
+  '</td>'+
+  '</tr>';
+}
+
+function obtenerObjetoBD(){
+  var obtenerInfoBD = firebase.database().ref('usuarios/');
+  obtenerInfoBD.on('child_added', function(data){
+    var persona = data.val();
+    var tabla = crearTabla(persona.id, persona.nombre, persona.apellido);
+    innerHTML('CargarDatos', tabla);
+  });
+}
+
+function innerHTML( id, contenidoTabla ){
+  return getID(id).innerHTML += contenidoTabla;
+}
+
+function eliminar( id ){
+  
+  //console.log("click en eliminar");
+  var usuario = firebase.database().ref('usuarios/'+id);
+
+  if (confirm('Desea eliminar este registro?')) {
+    usuario.remove();
+    location.reload();
+  }
+
+}
+
+function editar( id, nombre, apellido){
+  
+  console.log("click en editar");
+  // var usuario = firebase.database().ref('usuarios/'+id);
+  // usuario.remove();
+}
